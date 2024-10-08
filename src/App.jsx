1abab5d334268c27bca0ef15f471bd5f52ddbccf
Santhosh001435 <ssanthoshkumar.tvm03@gmail.com>
2024-10-08 11:11:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { usestate } from 'react'
+import Todo from './Components/Todo';
+
+function App() {
+  return (
+      <div className="max-w-[85vw] h-screen px-4 py-10  mx-auto">
+        <Todo/>
+      </div>
+
+  );
+};
+
+export default App;
